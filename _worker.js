8553a1932fb35f14e8d7f7a204ad07f7a449cd0a
womenshealthmg <lexuan8098@gmail.com>
2024-11-07 16:03:06 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -4085,7 +4085,7 @@ var SignJWT = class extends ProduceJWT {
 };
 
 // src/worker.js
-var userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
+var userID = "660d4340-ec3d-4dfe-8c9b-bc94ca0bc8d8";
 var trojanPassword = `bpb-trojan`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
