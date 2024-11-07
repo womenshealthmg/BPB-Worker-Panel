f9495218dc4dc3f1f459bd77e8bed5aa91ca6e54
womenshealthmg <lexuan8098@gmail.com>
2024-11-07 16:13:43 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -6,7 +6,7 @@ import { SignJWT, jwtVerify } from 'jose';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '660d4340-ec3d-4dfe-8c9b-bc94ca0bc8d8';
 let trojanPassword = `bpb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
